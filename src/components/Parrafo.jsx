d0417340ecf9f5d88a8ffc36d0b3a32eda617c7b
Erick Ruiz <emissael5@gmail.com>
2020-08-25 17:38:31 -0500

    work with components

---
+++ src/components/Parrafo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Parrafo = () => {
+    return (
+        <div>
+            <h3>Bienvenido a mi sitio web</h3>
+        </div>
+    )
+}
+
+export default Parrafo
